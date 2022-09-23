a20201ef6b825b0e2ca4832952895b25c368b7dd
Przemysław Czajka <zamaskowany@o2.pl>
2022-09-23 21:07:33 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+#Zadanie 3 1 1
+
+shop_list = {'warzywniak': ['jabłka', 'cebula', 'ziemniaki'],  
+                'mięsny': ['kiełbasa', 'wołowina']}
+
+x = 0
+x = (len(shop_list['warzywniak'])) + (len(shop_list['mięsny']))
