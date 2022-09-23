d7777540d77c31f68a5c8fc339981902db9c1156
Przemysław Czajka <zamaskowany@o2.pl>
2022-09-23 21:08:43 +0200

    Add main.py

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ shop_list = {'warzywniak': ['jabłka', 'cebula', 'ziemniaki'],
 
 x = 0
 x = (len(shop_list['warzywniak'])) + (len(shop_list['mięsny']))
+
+print("Lista zakupów")
+for (a, b) in shop_list.items():
+    len(a)
+    a = a.capitalize()
+    b = " ".join(b)
+    b = b.title()
+    print("Idę do", a, "i kupuję tam", b)
+print("W sumie kupuję", x, "produktów.")
